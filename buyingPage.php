948c2e14d9eab78b1c3266a7af0c54d71e693b0c
Kacper <kacperkapitrebacz@gmail.com>
2023-05-13 23:57:59 +0200

    Create interface of startPage and buyingPage

---
+++ buyingPage.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Buying Page</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+     <link rel="stylesheet/less" type="text/css" href="./style/main.less" />
+     <link rel="stylesheet/less" type="text/css" href="./style/buyingPage.less" />
+          
+</head>
+
+<body>
+
+     <main>
+
+          <div class="col-7">
+
+               <div class="row">
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+                    <div class="col-4 p-4 avaliableImgItem">
+                         <div class="content">
+                              <div class="img"></div>
+                              <div class="price">3.00</div>
+                         </div>
+                         <div class="name">
+                              Product name
+                         </div>
+                    </div>
+               </div>
+
+          </div>
+
+
+          <div class="col-5">
+
+               <div class="col-12 listItem">
+                    <section class="top">Product name</section>
+                    <section class="d-flex justify-content-between bottom">
+                         <span class="listItemPieces">1 piece</span>
+                         <span class="listItemPrice">3.00</span>
+                    </section>
+               </div>
+
+               <div class="col-12 listItem">
+                    <section class="top">Product name</section>
+                    <section class="d-flex justify-content-between bottom">
+                         <span class="listItemPieces">1 piece</span>
+                         <span class="listItemPrice">3.00</span>
+                    </section>
+               </div>
+
+          </div>
+
+     </main>
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+          crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+</body>
+
+</html>
